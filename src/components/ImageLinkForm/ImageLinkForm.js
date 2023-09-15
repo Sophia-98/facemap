ce21cc8f38177874ea442f853fb991a89735fbea
Sophia Panagiotaras <sophiapanagiotaras31@gmail.com>
2023-09-15 22:21:57 +0100

    error

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,22 +1,40 @@
-import './ImageLinkForm.css'
-const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
-    return(
-        
-        <div>
-            <p className="f3">
-                {'This magic brain will detect faces in your images. Give it a try.'}
-            </p>
-            <div className='center'>
-                <div className="form center pa4 br3 shadow-5">
-                    <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onButtonSubmit} >Detect</button>
-                </div>
-                
-            </div>
-        </div>
+import React, { useState } from 'react';
+import './ImageLinkForm.css';
 
-    );
-  
-}
- 
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleDetectClick = () => {
+    if (!document.getElementById('imageInput').value) {
+      setErrorMessage('Please upload an image.');
+    } else {
+      setErrorMessage('');
+      onButtonSubmit();
+    }
+  };
+
+  return (
+    <div>
+      <p className="f3">{'This magic brain will detect faces in your images. Give it a try.'}</p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input
+            id="imageInput"
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={handleDetectClick}
+          >
+            Detect
+          </button>
+        </div>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </div>
+    </div>
+  );
+};
+
 export default ImageLinkForm;
