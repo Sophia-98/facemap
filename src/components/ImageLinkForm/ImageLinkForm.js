2964d867b81f0d586a4669e354232b8a2690f0ee
Sophia Panagiotaras <sophiapanagiotaras31@gmail.com>
2023-09-15 22:35:24 +0100

    error

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -16,9 +16,9 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">{'This magic brain will detect faces in your images. Give it a try.'}</p>
+      {errorMessage && <p className="error-message red f6">{errorMessage}</p>}
       <div className="center">
         <div className="form center pa4 br3 shadow-5">
-        {errorMessage && <p className="error-message red f6">{errorMessage}</p>}
           <input
             id="imageInput"
             className="f4 pa2 w-70 center"
